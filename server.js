01d5630a2d1e15232d278c5566b7c6a3d7424ffe
Skyler-code101 <zeroskyler32@gmail.com>
2023-09-07 18:16:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { WebSocketServer } = require('ws')
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
+    console.log(req.socket.remoteAddress);
   ws.on('message', function message(data) {
     console.log('received: %s', data);
     wss.clients.forEach(function(client) {
